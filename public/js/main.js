a9d0baf10d068f6badf0178e1c925e672d7dd72d
rifkinni <rifkinni@gmail.com>
2016-04-19 17:16:00 -0400

    side bar!!

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
+var myVar = setInterval(updateFacts, 5000);
+var facts = ["Computer passwords are one example of codes", 
+"Learn more about different secret codes " +  setupLinks("here", "https://sites.google.com/site/codesforscouts/home"),
+"The field of making and breaking codes is called cryptography"];
+var index = 0;
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
+function setupLinks(str, link) {
+  var result = str.link(link);
+  return result
+}
+
+function updateFacts() {
+  document.getElementById("facts").innerHTML = facts[index];
+  index = (index + 1)%2;
 }
 
